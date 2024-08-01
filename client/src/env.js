666a77be3ba48ec87d662ba1d5cf6f66b67cc356
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-01 10:52:27 -0600

    Set up ENV

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-v6an8tmyb1oj3ssb.us.auth0.com'
-export const clientId = '0hCDMTdgWypFlRUpMixBIr40AV5acejf'
-export const audience = 'https://superZoomZoom.com/'
+export const domain = 'dev-2g4t3jrnjir8fwq2.us.auth0.com'
+export const clientId = 'eFDd9h3hiQdIqmBdEFnvf0TeNrPQkGTk'
+export const audience = 'https://thomauth0.com'
