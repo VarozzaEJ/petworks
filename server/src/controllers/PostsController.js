79a185aff8be36e7c5d51855f3cf614d98e69d42
Evan Varozza <VarozzaEJ@gmail.com>
2024-08-01 11:10:27 -0600

    creating posts in controller and service

---
+++ server/src/controllers/PostsController.js
@@ -0,0 +1,26 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
+import BaseController from "../utils/BaseController.js";
+import { postsService } from "../services/PostsService.js";
+
+
+export class PostsController extends BaseController {
+    constructor() {
+        super('api/posts')
+        this.router
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createPost)
+    }
+
+    async createPost(request, response, next) {
+        try {
+            const user = request.userInfo
+            const postData = request.body
+            postData.creatorId = user.id
+            const newPost = await postsService.createPost(postData)
+            response.send(newPost)
+        }
+        catch (error) {
+            next(error);
+        }
+    }
+}
---
+++ server/src/services/PostsService.js
@@ -0,0 +1,11 @@
+import { dbContext } from "../db/DbContext.js"
+
+
+class PostsService {
+    async createPost(postData) {
+        const post = await dbContext
+    }
+
+}
+
+export const postsService = new PostsService()
