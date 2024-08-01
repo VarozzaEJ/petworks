239be73a7eb151431dc45de38aad5de6d53c3694
jo <josehehall98@gmail.com>
2024-08-01 11:12:05 -0600

    dbContext for Post

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { PetSchema } from '../models/Pet.js';
+import { PostSchema } from '../models/Post.js';
+
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-
+  Posts = mongoose.model('Post', PostSchema);
   Pets = mongoose.model('Pet', PetSchema)
 }
 
