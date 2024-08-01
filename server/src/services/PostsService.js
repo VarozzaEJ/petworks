b5885bcb33b44d5c8b9b97a0e519fc335eec9031
jo <josehehall98@gmail.com>
2024-08-01 12:52:42 -0600

    Did nothing

--- server/src/services/PostsService.js
+++ server/src/services/PostsService.js
@@ -2,15 +2,16 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class PostsService {
-    async getAllPosts() {
-        const posts = await dbContext.Posts.find().populate('creator')
-        return posts
-    }
-    async createPost(postData) {
-        const post = await dbContext.Posts.create(postData)
-        await post.populate('creator')
-        return post
-    }
+  async createPost(postData) {
+    const post = await dbContext.Posts.create(postData)
+    await post.populate('creator')
+    return post
+  }
+  async getAllPosts() {
+    const posts = await dbContext.Posts.find().populate('creator')
+    return posts
+  }
+
 
 }
 
