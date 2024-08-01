b99aa8d2c601e2399b77bf0b123342516036b410
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-01 10:10:34 -0600

    Tried to fix env

--- client/src/env.js
+++ client/src/env.js
@@ -3,4 +3,4 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-v6an8tmyb1oj3ssb.us.auth0.com'
 export const clientId = '0hCDMTdgWypFlRUpMixBIr40AV5acejf'
-export const audience = 'https://superzoomzoom.com/'
+export const audience = 'https://superZoomZoom.com/'
