fc67d0a4e9c24f279ac7f1bb6155574f9638fc3b
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-07 10:32:56 -0600

    Pet of the Day card is finished

--- client/src/components/PetOfTheDay.vue
+++ client/src/components/PetOfTheDay.vue
@@ -33,23 +33,28 @@ defineProps({ petOfTheDayProp: Pet })
       </div>
     </div>
   </div>
-  <div class="card d-block d-sm-none m-3 bg-info" style="width: 100dvh;">
+
+
+  <div class="card d-block d-sm-none my-3 bg-info shadow" style="width: 100dvh;">
     <div class="card-body">
       <div>
         <p class="card-title text-center fs-3 fw-semibold mb-3">Pet of the Day</p>
         <hr class="mb-2">
         <p class="card-text text-capitalize fs-4 fw-semibold">{{ petOfTheDayProp.name }}</p>
       </div>
-      <small class="card-text text-capitalize fw-semibold text-center">{{ petOfTheDayProp.bio }}</small>
-      <div class="d-flex mt-4">
-        <p class="me-auto card-text">Owners:</p>
+      <div class="text-center">
+        <small class="card-text text-capitalize fw-semibold">{{ petOfTheDayProp.bio }}</small>
+      </div>
+    </div>
+    <img :src="petOfTheDayProp.imgUrl" class="img-fluid" :alt="petOfTheDayProp.name">
+    <div class="card-footer">
+      <div class="d-flex mt-2">
         <p v-for="owner in petOfTheDayProp.owner" :key="owner.id" class="card-text">
           <img class="profile-img me-1" :src="owner.picture" :alt="owner.name">
           <small class="text-body-secondary">{{ owner.name }}</small>
         </p>
       </div>
     </div>
-    <img :src="petOfTheDayProp.imgUrl" class="card-img-bottom" :alt="petOfTheDayProp.name">
   </div>
 </template>
 
