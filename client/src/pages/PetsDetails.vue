49282b4533d8e233714476f08cdb54728121ec99
ThomasWSnider <thomas.wayne.snider@gmail.com>
2024-08-08 15:42:08 -0600

    Organized petDetails code

--- client/src/pages/PetsDetails.vue
+++ client/src/pages/PetsDetails.vue
@@ -39,31 +39,25 @@ async function getPetPosts() {
 <template>
   <section class="container-fluid" v-if="activePet">
     <div class="row">
-
       <div class="col-12 px-0">
         <img class="img-fluid pet-img" :src="activePet.imgUrl" alt="">
       </div>
       <div class="col-12 pt-4 bg-primary d-flex flex-column align-items-center ">
         <p class="text-capitalize fs-1 fw-bold">{{ activePet.name }}</p>
         <p class="fs-2 text-capitalize fw-bold">Species: {{ activePet.species }}</p>
-
-
         <div v-for="activePet in activePet.petStats" :key="activePet.attribute"
           class="d-flex w-100  mb-4 row justify-content-between bg-light">
           <div class="row">
-
             <div class="col-4 d-flex me-4">
               <p class="bg-subtle d-flex mb-0 me-3 p-1">{{ activePet.attribute }}: </p>
             </div>
             <div class="col-7 pe-3 justify-content-between d-flex">
-
               <div v-if="activePet.value == 1" class="bg-light  d-flex">
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 me-2 d-flex justify-content-center align-items-center"></div>
-
               </div>
               <div v-else-if="activePet.value == 2" class="bg-light  d-flex">
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
@@ -71,7 +65,6 @@ async function getPetPosts() {
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 me-2 d-flex justify-content-center align-items-center"></div>
-
               </div>
               <div v-else-if="activePet.value == 3" class="bg-light d-flex">
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
@@ -79,7 +72,6 @@ async function getPetPosts() {
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 me-2 d-flex justify-content-center align-items-center"></div>
-
               </div>
               <div v-else-if="activePet.value == 4" class="bg-light d-flex">
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
@@ -87,7 +79,6 @@ async function getPetPosts() {
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle bg-primary ms-1 d-flex justify-content-center align-items-center"></div>
                 <div class="move-down handle ms-1 me-2 d-flex justify-content-center align-items-center"></div>
-
               </div>
               <div v-else-if="activePet.value == 5" class="bg-light  d-flex">
                 <div class="move-down handle bg-primary ms-1 d-flex  align-items-center"></div>
@@ -99,19 +90,16 @@ async function getPetPosts() {
             </div>
           </div>
         </div>
-
         <div class=" pt-3 d-flex justify-content-center">
           <p class="fs-5">{{ activePet.bio }}</p>
         </div>
       </div>
-
       <div class="d-flex justify-content-center pt-3">
-
         <p class="d-inline-flex gap-1">
-
           <button class="btn btn-subtle-outline" type="button" data-bs-toggle="collapse"
-            data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Posts <i
-              class="mdi mdi-menu-down-outline"></i></button>
+            data-bs-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">
+            Posts <i class="mdi mdi-menu-down-outline"></i>
+          </button>
         </p>
       </div>
       <div class="col-12 collapse multi-collapse" id="multiCollapseExample2">
