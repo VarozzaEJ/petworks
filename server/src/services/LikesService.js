ce2d42bf5820561df04dff86810746d0b8a2085f
jo <josehehall98@gmail.com>
2024-08-09 10:24:24 -0600

    pull

--- server/src/services/LikesService.js
+++ server/src/services/LikesService.js
@@ -2,12 +2,18 @@ import { dbContext } from "../db/DbContext.js"
 
 
 class LikesService {
-    async likePost(likeData) {
-        const likedPost = await dbContext.Likes.create(likeData)
-        await likedPost.populate('post account')
-        await likedPost.save()
-        return likedPost
-    }
+  async likePost(likeData) {
+    const likedPost = await dbContext.Likes.create(likeData)
+    await likedPost.populate('post account')
+    await likedPost.save()
+    return likedPost
+  }
+
+
+
+  async unLike() {
+
+  }
 }
 
 export const likesService = new LikesService()
